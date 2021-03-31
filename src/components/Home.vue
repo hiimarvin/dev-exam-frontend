d204f0228dfef767cd1395a60dc3b9ed2407b065
hiimarvin <arvincmedina@gmail.com>
2021-04-01 02:11:44 +0800

    Initial solution

---
+++ src/components/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-container>
+    <v-data-table
+      :headers="tableHeaders"
+      :items="tableData"
+      :items-per-page="5"
+      :loading="isTableLoading"
+      hide-default-footer
+      disable-pagination>
+      <template v-slot:top>
+        <v-btn
+          block
+          outlined
+          @click="addRow"
+        >Add Row</v-btn>
+      </template>
+      <template v-slot:item.http_res="{ item }" >
+        <pre>
+          {{ item.http_res }}
+        </pre>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      tableHeaders: [
+        {
+          text: 'Date/Time',
+          value: 'date_time',
+          align: 'start',
+          width: 170
+        },
+        { text: 'HTTP Response',
+          value: 'http_res',
+          sortable: false
+        }
+      ],
+      tableData: [],
+      isTableLoading: true
+    }
+  },
+  methods: {
+    getTableData() {
+      axios.get('http://localhost:8082/api/httpRes')
+        .then(response => {
+          this.tableData = response.data;
+          this.isTableLoading = false;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    addRow() {
+      axios.get('http://localhost:8081/datetime')
+        .then(response => {
+          this.isTableLoading = true;
+          return axios.post('http://localhost:8082/api/httpRes/store', {
+            'date_time': response.data.date_time,
+            'http_res': JSON.stringify(response)
+          });
+        })
+        .then(() => {
+          return this.getTableData();
+        });
+    }
+  },
+  created() {
+    this.getTableData();
+  }
+}
+</script>
